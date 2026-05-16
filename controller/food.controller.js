50df0c3874e1d706b5853377f7c5d8f311affda5
Adedayo <atimileyin911@gmail.com>
2026-05-16 21:34:20 +0100

    FEAT:handle delete

--- controller/food.controller.js
+++ controller/food.controller.js
@@ -62,7 +62,7 @@ const handleGetAllFoods = async(req,res,next)=>{
         return next(new customError("un-authorizied user",400))
     }
     try {
-        const allMeals = await FOOD.find({user:userId})
+        const allMeals = await FOOD.find({user:userId}).sort({ day: 1 }) 
          if (!allMeals.length) {
             return next(new customError("No meals yet", 404))
         }
@@ -81,4 +81,55 @@ const handleGetAllFoods = async(req,res,next)=>{
     }
 }
 
-module.exports = {handleCreateFood,handleGetAllFoods}
+const handleUpdateMeal = async (req, res, next) => {
+    const { id } = req.params
+    const userId = req.user._id
+    const { name, day, mealPeriod } = req.body
+
+    try {
+        const meal = await FOOD.findOne({ _id: id, user: userId })
+        if (!meal) {
+            return next(new customError("Meal not found", 404))
+        }
+
+        const updatedMeal = await FOOD.findOneAndUpdate(
+            { _id: id, user: userId },
+            { name, day, mealPeriod },
+            { new: true, runValidators: true }
+        )
+
+        return res.status(200).json({
+            success: true,
+            message: "Meal updated",
+            updatedMeal
+        })
+    } catch (error) {
+        return next(error)
+    }
+}
+
+const handleDeleteMeal = async (req, res, next) => {
+    const { id } = req.params
+    const userId = req.user._id
+
+    if (!id) {
+        return next(new customError("Invalid request", 400))
+    }
+
+    try {
+        const deletedMeal = await FOOD.findOneAndDelete({ _id: id, user: userId })
+
+        if (!deletedMeal) {
+            return next(new customError("Meal does not exist", 404))
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Meal has been removed"
+        })
+    } catch (error) {
+        return next(error)
+    }
+}
+
+module.exports = {handleCreateFood,handleGetAllFoods,handleUpdateMeal,handleDeleteMeal}
--- routes/food.route.js
+++ routes/food.route.js
@@ -1,8 +1,9 @@
 const route = require("express").Router()
 const tokenVerification = require("../middleware/Auth.middleware")
-const {handleCreateFood,handleGetAllFoods} = require("../controller/food.controller")
+const {handleCreateFood,handleGetAllFoods,handleUpdateMeal, handleDeleteMeal} = require("../controller/food.controller")
 
 route.post("/create",tokenVerification,handleCreateFood)
 route.get("/getAll", tokenVerification ,handleGetAllFoods)
-
+route.patch("/Update/:id", tokenVerification, handleUpdateMeal)
+route.delete("/delete/:id",tokenVerification,handleDeleteMeal)
 module.exports = route
