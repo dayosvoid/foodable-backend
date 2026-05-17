dafe494d6a3bd8cc6f5ed90bd6699a4a4ec1ca80
Adedayo <atimileyin911@gmail.com>
2026-05-17 07:14:12 +0100

    cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ PORT = process.env.PORT || 2000
 app.use(express.json())
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:process.env.FONTEND_URL,
     credentials:"true"
 }))
 app.use("/api/foodable/food", foodRoute)
